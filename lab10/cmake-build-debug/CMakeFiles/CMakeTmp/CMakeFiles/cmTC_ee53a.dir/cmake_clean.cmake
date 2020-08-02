b28266436391a59aebf9bb4eefb7ae16f37ea229
cmak8888 <67659799+cmak8888@users.noreply.github.com>
2020-08-02 17:42:26 -0600

    Add files via upload

---
+++ lab10/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ee53a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_ee53a.dir/testCCompiler.c.obj"
+  "cmTC_ee53a.pdb"
+  "cmTC_ee53a.exe"
+  "cmTC_ee53a.exe.manifest"
+  "cmTC_ee53a.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_ee53a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
