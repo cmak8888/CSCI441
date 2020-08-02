bf5e25adb708d17305e25c2eb14165efd2cb8217
cmak8888 <67659799+cmak8888@users.noreply.github.com>
2020-08-02 16:34:10 -0600

    Add files via upload

---
+++ lab00/lab00_clion/lab00a/cmake-build-debug/CMakeFiles/lab00a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab00a.dir/main.cpp.obj"
+  "lab00a.pdb"
+  "lab00a.exe"
+  "lab00a.exe.manifest"
+  "liblab00a.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab00a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab00/lab00_clion/lab00b/cmake-build-debug/CMakeFiles/lab00b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab00b.dir/main.cpp.obj"
+  "lab00b.pdb"
+  "lab00b.exe"
+  "lab00b.exe.manifest"
+  "liblab00b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab00b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
