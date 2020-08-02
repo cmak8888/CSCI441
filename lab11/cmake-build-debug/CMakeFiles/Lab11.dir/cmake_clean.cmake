a636345b082eb59057884d6c0cc785f3e2d1eb59
cmak8888 <67659799+cmak8888@users.noreply.github.com>
2020-08-02 17:54:04 -0600

    Add files via upload

---
+++ lab11/cmake-build-debug/CMakeFiles/Lab11.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab11.dir/main.cpp.obj"
+  "CMakeFiles/Lab11.dir/src/Marble.cpp.obj"
+  "Lab11.pdb"
+  "Lab11.exe"
+  "Lab11.exe.manifest"
+  "libLab11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab11/readme.txt
@@ -0,0 +1,8 @@
+Q1: The marbles moves straight and flat, forever.
+Q2: Yes. <-1,0,0>, <1,0,0>, <0,0,-1> and <0,0,1>.
+Q3: N1= L1 - L2. N2 = L2 - L1. Normalize the distance between 2 points to get the vector normal to the collision plane.
+Q4: Yes.
+Q5: 8 .
+Q6: Just Right.
+Q7: 2 hours.
+Q8: None.
